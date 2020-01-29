53aaabf85945c60fbd62616b589da3f86711607d
mthomas-ketchbrook <52801195+mthomas-ketchbrook@users.noreply.github.com>
2020-01-29 16:57:53 -0500

    Major updates

--- app.R
+++ app.R
@@ -1,11 +1,15 @@
 # Shiny app that allows users to enter their home info and 
 
 library(shiny)
+library(shinydashboard)
 library(tidyverse)
+library(glue)
 
 modl_data_fnl <- readRDS("modl_data_fnl.RDS")
 lm_modl <- readRDS("lm_modl.RDS")
+lm_train_data <- readRDS("lm_train_data.RDS")
 xgb_modl <- readRDS("xgb_modl.RDS")
+xgb_train_data <- readRDS("xgb_train_data.RDS")
 source("funs.R")
 
 
@@ -26,69 +30,103 @@ cpi_data <- macro.env$CPIAUCSL %>%
 # Build App ---------------------------------------------------------------
 
 ui <- fluidPage(
-  shiny::column(
-    width = 4, 
-    shiny::wellPanel(
-      shiny::sliderInput(
-        inputId = "bedrooms_slider", 
-        label = "Number of Bedrooms", 
-        min = 1, 
-        max = 6, 
-        value = 3, 
-        step = 1
-      ), 
-      shiny::sliderInput(
-        inputId = "bathrooms_slider", 
-        label = "Number of Bathrooms", 
-        min = 1, 
-        max = 7, 
-        value = 2, 
-        step = 0.5
-      ), 
-      shinyWidgets::pickerInput(
-        inputId = "city_picker", 
-        label = "Select City", 
-        choices = c(unique(modl_data_fnl$city))
-      ), 
-      shiny::numericInput(
-        inputId = "sqft_input", 
-        label = "Enter Square Footage", 
-        value = 1800, 
-        min = 400, 
-        max = 10000
-      ), 
-      shiny::numericInput(
-        inputId = "year_built_input", 
-        label = "Enter Year Built", 
-        value = 2000, 
-        min = 400, 
-        max = 10000
-      ), 
-      # shiny::dateInput(
-      #   inputId = "date_sold_input", 
-      #   label = "Enter Date of Sale \n (or leave unchanged if you would like to simulate sale as of today's date)", 
-      #   value = lubridate::today()
-      # ), 
-      shiny::actionButton(
-        inputId = "predict_button", 
-        label = "Generate Prediction", 
-        icon = shiny::icon("download")
-      )#,
-      # shiny::verbatimTextOutput(
-      #   "season"
-      # )
+  title = "Home Sale Price Estimator for Tolland County, Connecticut", 
+  
+  shiny::HTML("<div id=\"ketchbrook_banner\" class=\"shiny-image-output\" style=\"width: 100% ; height: 150px\"></div>"), 
+  
+  shiny::div(
+    shiny::h1("Tolland County, Connecticut Home Sale Price Estimator"), 
+    shiny::p("Developed by Ketchbrook Analytics")
+  ), 
+  
+  shiny::div(
+    shiny::column(
+      width = 4, 
+      shiny::wellPanel(
+        shiny::sliderInput(
+          inputId = "bathrooms_slider", 
+          label = "Number of Bathrooms", 
+          min = 1, 
+          max = 7, 
+          value = 2, 
+          step = 0.5
+        ), 
+        shinyWidgets::pickerInput(
+          inputId = "city_picker", 
+          label = "Select City", 
+          choices = c(unique(modl_data_fnl$city))
+        ), 
+        shiny::numericInput(
+          inputId = "sqft_input", 
+          label = "Enter Square Footage", 
+          value = 1800, 
+          min = 400, 
+          max = 10000
+        ), 
+        shiny::numericInput(
+          inputId = "year_built_input", 
+          label = "Enter Year Built", 
+          value = 2000, 
+          min = 400, 
+          max = 10000
+        ), 
+        # shiny::dateInput(
+        #   inputId = "date_sold_input", 
+        #   label = "Enter Date of Sale \n (or leave unchanged if you would like to simulate sale as of today's date)", 
+        #   value = lubridate::today()
+        # ), 
+        shiny::actionButton(
+          inputId = "predict_button", 
+          label = "Generate Prediction", 
+          icon = shiny::icon("download")
+        )#,
+        # shiny::verbatimTextOutput(
+        #   "season"
+        # )
+      )
     )
   ), 
-  shiny::column(
-    width = 8, 
-    DT::DTOutput(
-      "temp_tbl"
+  
+  shiny::div(
+    shiny::column(
+      width = 8, 
+      shinydashboard::valueBoxOutput(
+        "pred"
+      ), 
+      shiny::div(
+        shiny::tabsetPanel(
+          shiny::tabPanel(
+            "Linear Regression", 
+            shiny::plotOutput(
+              "lime_plot_lm"
+            )
+          ), 
+          shiny::tabPanel(
+            "XGBoost", 
+            shiny::plotOutput(
+              "lime_plot_xgb"
+            )
+          )
+        )
+      )
     )
   )
 )
 
 
 server <- function(input, output, session) {
+  
+  output$ketchbrook_banner <- shiny::renderImage({
+    
+    list(
+      src = "img/Ketchbrook_Logo_nobackground.png", 
+      contentType = "image/png", 
+      Width = 1000, 
+      height = (1000 * 58) / 368,
+      alt = "KetchbrookLogo"
+    )
+    
+  }, deleteFile = F)
   
   output$season <- renderText({
     dplyr::case_when(
@@ -100,9 +138,9 @@ server <- function(input, output, session) {
     
   })
   
-  df_eval <- shiny::reactive({
+  df_eval <- shiny::eventReactive( input$predict_button, {
     
-    data.frame(
+    tibble::tibble(
       city = input$city_picker, 
       bathrooms = input$bathrooms_slider, 
       finishedSqFt = input$sqft_input, 
@@ -116,12 +154,53 @@ server <- function(input, output, session) {
       cpipctchange = cpi_data$cpipctchange
     )
     
+  }, ignoreNULL = FALSE)
+  
+  output$pred <- shinydashboard::renderValueBox({
+    
+    shinydashboard::valueBox(
+      "Title", 
+      glue::glue(
+        "Today's Estimated Sale Price: ${format(generate_ensemble_pred(model1 = lm_modl, model2 = xgb_modl, data = df_eval()), big.mark = \",\")}"
+      ), 
+      icon = shiny::icon("credit-card")
+    )
+    
   })
   
   
-  output$temp_tbl <- DT::renderDataTable(
-    df_eval()
-  )
+  # output$pred <- shiny::renderText({
+  #   
+  #   glue::glue(
+  #     "Today's Estimated Sale Price: ${format(generate_ensemble_pred(model1 = lm_modl, model2 = xgb_modl, data = df_eval()), big.mark = \",\")}"
+  #   )
+  #   
+  # })
+  
+  output$lime_plot_lm <- shiny::renderPlot({
+    
+    generate_lime_chart(
+      model = lm_modl, 
+      train_data = lm_train_data, 
+      new_data = df_eval()
+    )
+    
+  })
+  
+  output$lime_plot_xgb <- shiny::renderPlot({
+    
+    generate_lime_chart(
+      model = xgb_modl, 
+      train_data = xgb_train_data, 
+      new_data = df_eval()
+    )
+    
+  })
+  
+  
+  
+  
+  
   
   # output$predicted_value <- shiny::renderPrint({
   # 
