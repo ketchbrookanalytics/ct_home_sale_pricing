10206b65594e5041b66fb0aa2d230501ddb7e97a
Ivan Millanes <ivanmillanes26@gmail.com>
2024-06-10 17:48:39 -0300

    Center nav bar titles vertically

--- golem.refactor/R/app_ui.R
+++ golem.refactor/R/app_ui.R
@@ -10,6 +10,7 @@ app_ui <- function(request) {
     golem_add_external_resources(),
     # Your application UI logic
     bslib::page_navbar(
+      id = "app_navbar",
       theme = bslib::bs_theme(
         bootswatch = "sandstone",
         fg = "#000000",
